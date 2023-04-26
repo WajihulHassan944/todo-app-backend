3cc2ebd341a2ec63a3b14ad68d1506dc50d5c3dc
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-04-26 10:54:34 +0500

    Updated minor changes

--- routes/task.js
+++ routes/task.js
@@ -5,17 +5,11 @@ import {
   newTask,
   updateTask,
 } from "../controllers/task.js";
-import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/new", newTask);
 
 router.get("/my", getMyTask);
-
-router
-  .route("/:id")
-  .put(isAuthenticated, updateTask)
-  .delete(isAuthenticated, deleteTask);
 
 export default router;
