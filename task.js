a71b5dea71331efe40e8424a5981673460ab730a
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-04-26 10:07:58 +0500

    Add files via upload

---
+++ task.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  deleteTask,
+  getMyTask,
+  newTask,
+  updateTask,
+} from "../controllers/task.js";
+
+const router = express.Router();
+
+router.post("/new",  newTask);
+
+router.get("/my", getMyTask);
+
+router
+  .route("/:id")
+  .put(isAuthenticated, updateTask)
+  .delete(isAuthenticated, deleteTask);
+
+export default router;
